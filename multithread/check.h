08080d2f384059f0d61de436b0dde6578119ac43
visl <visl@visl-Precision-WorkStation-T5400.(none)>
2013-11-05 11:00:57 -0800

    add mutex lock.

---
+++ multithread/check.h
@@ -0,0 +1,11 @@
+#ifndef __CHECK_H
+#define __CHECK_H
+
+#define checkResults(string, val) {             \
+ if (val) {                                     \
+   printf("Failed with %d at %s", val, string); \
+   exit(1);                                     \
+ }                                              \
+}
+
+#endif
