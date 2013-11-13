96d7b5ac3d29a89128992e601e030594e531d15b
visl <visl@visl-Precision-WorkStation-T5400.(none)>
2013-11-12 16:26:23 -0800

    more basic

--- c-basic/seq.c
+++ c-basic/seq.c
@@ -5,4 +5,7 @@ int main(void)
     int i = 8;
     printf("%d, %d, %d\n", i++, i++, i++);
     printf("%d, %d, %d\n", ++i, ++i, ++i);
+    printf("%d\n", i);
+    printf("%d, %d \n", -i++, -i--);
+    printf("%d\n", i);
 }
