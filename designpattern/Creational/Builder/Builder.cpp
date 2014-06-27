e106bad2f883f87487cfcc0f3fc658c30583918a
Henry Tang <yutang2001@gmail.com>
2014-06-26 21:58:04 -0700

    Add builder

--- designpattern/Creational/Builder/Builder.cpp
+++ designpattern/Creational/Builder/Builder.cpp
@@ -4,40 +4,42 @@ using namespace std;
 
 class Product {
     public:
-       Product();
-       ~Product();
-       void ProductPart();
+       Product(){};
+       ~Product(){};
+       void ProductPart(){};
 };
 
 class ProductPart {
     public:
-       ProductPart();
-       ~ProductPart();
-       ProductPart *BuildPart();
+       ProductPart(){};
+       ~ProductPart(){};
+       ProductPart *BuildPart() { return NULL; };
 };
 
+/*
 class Component {
     public:
        void Add( const Component & com) {} 
        Component *GetChild(int index) { return 0; }
        void Component::Remove( const Component & com) {}
+*/
 
 class Builder {
     public:
-         virtual ~Builder();
+         virtual ~Builder(){};
          virtual void BuildPartA(const string &buildPara) = 0;
          virtual void BuildPartB(const string &buildPara) = 0;
          virtual void BuildPartC(const string &buildPara) = 0;
          virtual Product *GetProduct() = 0;
    protected:
-         Builder(); 
+         Builder(){}; 
 };
 
 class ConcreteBuilder: public Builder
 {
    public:
-         ConcreteBuilder();
-         ~ConcreteBuilder();
+         ConcreteBuilder(){};
+         ~ConcreteBuilder(){};
          void BuildPartA(const string & buildPara) { cout << "Build PartA" << endl; }
          void BuildPartB(const string & buildPara) { cout << "Build PartB" << endl; }
          void BuildPartC(const string & buildPara) { cout << "Build PartC" << endl; }
@@ -50,5 +52,28 @@ class ConcreteBuilder: public Builder
          };
 };
        
-    
+ 
+class Director {
+    public:
+       Director(Builder *bld) { _bld = bld; }
+       ~Director(){};
+       void Construct() 
+       {
+           _bld -> BuildPartA("user_defined");
+           _bld -> BuildPartB("user_defined");
+           _bld -> BuildPartC("user_defined");
+       };
+    private:
+       Builder *_bld;
+};
+
+
+int main(void)
+{
+      Director *d = new Director(new ConcreteBuilder());
+      d -> Construct();
+      return 0;
+}
+
+   
 
